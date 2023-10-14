81b915f6d67c69152793b239e69d665f28290e0b
serg <seruyvodoley@gmail.com>
2023-10-14 10:07:24 +0700

    стабильно

--- search.py
+++ search.py
@@ -1,7 +1,8 @@
 """Модуль поиска подстроки в строке"""
 import argparse
+import hashlib
 import time
-
+from colorama import Fore
 
 def time_counter(function):
     def do_time_count(*args, **kwargs):
@@ -14,10 +15,22 @@ def time_counter(function):
     return do_time_count
 
 
+def hashh(input_string):
+    """
+    переопределение метода hash()
+    :param input_string: входная строка
+    :return: хэш строки
+    """
+    sha256 = hashlib.sha256()
+    sha256.update(input_string.encode('utf-8'))
+    hash_value = sha256.hexdigest()
+    return hash_value
+
+
 def rabin_karp(string, substring, method, count):
     """
     Функция Рабина-Карпа
-    :param string: Строка в которой будет производиться поиск
+    :param string: Строка в которой буддет производиться поиск
     :param substring: Подстрока, которая будет искаться в строке
     :param method: Направление поиска подстроки в строке
     :param count: Количество вхождений подктроки в строку
@@ -26,20 +39,24 @@ def rabin_karp(string, substring, method, count):
     string_len, substring_len = len(string), len(substring)
     result_list = []
     start, stop, step = 0, string_len - substring_len + 1, 1
-    hash_substring = hash(substring)
+    hash_substring = hashh(substring)
     if method == 'last':
         start, stop, step = len(string) - 1, 0 + len(substring) - 2, -1
+        # len(string) - 1 устанавливает начальную позицию в конце строки
+        # означает, что итерация будет продолжаться до позиции len(substring) - 1.
+        # 0 + len(substring) - 2То есть, мы итерируемся по подстроке, и однако, чтобы включить последний символ,
+        # мы вычитаем 1
         substring = ''.join(reversed(substring))
-        hash_substring = hash(substring)
+        hash_substring = hashh(substring)
     if string_len >= substring_len:
         for i in range(start, stop, step):
             if method == 'first':
-                slice_stop = i + substring_len
+                slice_stop = i + substring_len  # для извлечения подстроки
             else:
                 slice_stop = i - substring_len
                 if slice_stop < 0:
                     slice_stop = None
-            hs = hash(string[i:slice_stop:step])
+            hs = hashh(string[i:slice_stop:step])  # вычислим хэш сегмента строки
             if hs == hash_substring:
                 if string[i:slice_stop:step] == substring:
                     if method == 'first':
@@ -61,12 +78,12 @@ def search(string=None, substring=None, case_sensitivity=True,
            method='first', count=1, file_path=None):
     """
     Функция для поиска подстроки в строке при помощи функции Рабина-Карпа
-    :param string: Строка в которой буддет производиться поиск
+    :param string: Строка в которой будет производиться поиск
     :param substring: Последовательность из подстрок или одна подстрока для поиска в строке
     :param case_sensitivity: Флаг чувствительности к регистру
     :param method: Направление поиска подстроки в строке
-    :param count: Количество вхождений подктроки в строку
-    :param file_path: Файл, в котором будет производиться поиск строки
+    :param count: Количество вхождений подстроки в строку
+    :param file_path: Файл, в котором будет производиться поиск
     :return: Количество индексов с вхождениями подстроки в строке или словарь
     """
     if file_path:
@@ -89,16 +106,11 @@ def search(string=None, substring=None, case_sensitivity=True,
         return substring_dict
 
 
-
 def main():
-    """
-    модуль управления
-    :return: None
-    """
     parser = argparse.ArgumentParser(description='Алгоритм Рабина-Карпа')
     parser.add_argument('-string', type=str, help='Строка для поиска')
     parser.add_argument('-substr', type=str, help='Подстрока или кортеж подстрок для поиска', nargs='*')
-    parser.add_argument('-case_sensitivity', type=bool, help='Чувствительность к регистру', default=True)
+    parser.add_argument('-case_sensitivity', type=bool, help='Чувствительность к регистру', default=False)
     parser.add_argument('-method', type=str, help='Направление поиска подстроки в строке', default='first')
     parser.add_argument('-count', type=int, help='Количество вхождений подстроки в строку', default=1)
     parser.add_argument('-file_path', type=str, help='Файл, в котором будет производиться поиск строки', default=None)
@@ -110,29 +122,35 @@ def main():
         else:
             args.substr = tuple(args.substr)
 
-    result = search(string=args.string, substring=args.substr, case_sensitivity=args.case_sensitivity,
-                    method=args.method, count=args.count, file_path=args.file_path)
-
-    if isinstance(result, dict):
-        for key, value in result.items():
-            if value:
-                print(f'Строка: {args.string}')
-                print(f'Подстрока: {key}')
-                print(f'Результат: {value}')
-                if len(value) > 10:
-                    print(f'первые 10 индексов: {value[:10]}')
-                else:
-                    print(f'Индексы: {value}')
+    if args.file_path:
+        with open(args.file_path, 'r') as file:
+            lines = file.readlines()[:10]
+            string = "".join(lines)
     else:
-        print(f'Строка: {args.string}')
+        string = args.string
+
+    results = search(string=string, substring=args.substr, case_sensitivity=args.case_sensitivity,
+                     method=args.method, count=args.count, file_path=args.file_path)
+    if isinstance(results, dict):
+        print(f'Строка: {string}')
+        print(f'Подстроки: {str(args.substr)}')
+        print('Результат:')
+
+        for key, value in results.items():
+            if value:
+                print(f"'{key}': {value}")
+                if len(value) > 10:
+                    print(f'Первые 10 индексов: {value[:10]}')
+            else:
+                print(f"'{key}': Not Found")
+    else:
+        print(f'Строка: {string}')
         print(f'Подстрока(и): {str(args.substr)}')
-        print(f'Результат: {result}')
-        if result and len(result) > 10:
-            print(f'First 10 indexes: {result[:10]}')
-        else:
-            print(f'Indexes: {result}')
+        print(f'Результат: {results}')
+        if results and len(results) > 10:
+            print(f'Первые 10 индексов: {results[:10]}')
 
 
 if __name__ == '__main__':
+    # colorama.init()
     main()
-
